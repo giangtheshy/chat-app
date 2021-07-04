3840b82887943e8ac65d116e55f2e94cb8aefc88
giangtheshy <72980294+giangtheshy@users.noreply.github.com>
2021-07-04 18:05:49 +0700

    add:some config nginx

--- client/src/pages/Call/Call.tsx
+++ client/src/pages/Call/Call.tsx
@@ -5,17 +5,21 @@ import { FiPhoneOff } from "react-icons/fi";
 import { answerCall, callUser, leaveCall, setCall } from "../../store/actions/call.action";
 import { RootState } from "../../store/reducers";
 import { ICall } from "../../store/reducers/call.reducer";
+import Peer from "simple-peer";
 import "./Call.scss";
+import { User } from "../../types";
 
 const Call = () => {
   const call: ICall = useSelector((state: RootState) => state.call);
   const socket = useSelector((state: RootState) => state.call.socket);
+  const user: User = useSelector((state: RootState) => state.user.user);
 
   const history = useHistory();
   const search = useLocation().search;
 
   const id: any = new URLSearchParams(search).get("id");
   const answer: any = new URLSearchParams(search).get("answer");
+
   const myVideo = useRef<any>();
   const userVideo = useRef<any>();
   const connectionRef = useRef<any>();
@@ -52,16 +56,83 @@ const Call = () => {
     }
   }, [answer, id]);
 
+  function answerCall() {
+    dispatch(setCall({ callAccepted: true }));
+
+    const peer = new Peer({ initiator: false, trickle: false, stream: call.stream });
+
+    peer.on("signal", (data) => {
+      console.log({ data });
+
+      call.socket?.emit("answerCall", { signal: data, to: call.call?.from });
+    });
+
+    peer.on("stream", (currentStream) => {
+      console.log({ currentStreamAnswer: currentStream });
+
+      userVideo.current.srcObject = currentStream;
+    });
+    console.log({ signalAnswer: call.call?.signal });
+
+    peer.signal(call.call?.signal);
+
+    connectionRef.current = peer;
+  }
+
+  // Hàm này được thực hiện khi click nút gọi
+  function callUser() {
+    const peer = new Peer({ initiator: true, trickle: false, stream: call.stream });
+
+    peer.on("signal", (data) => {
+      console.log({ data });
+      socket?.emit("callUser", {
+        userToCall: id,
+        signalData: data,
+        from: call.me,
+        name: user.name,
+        avatar: user.avatar,
+      });
+    });
+
+    // Sự kiện này không được gọi khi partners bấm nút trả lời
+    peer.on("stream", (currentStream) => {
+      console.log({ currentStream });
+
+      userVideo.current.srcObject = currentStream;
+    });
+
+    // Sự kiện này được gọi khi partners bấm nút trả lời
+    socket?.on("callAccepted", (signal) => {
+      console.log({ signal });
+
+      peer.signal(signal);
+      dispatch(setCall({ callAccepted: true }));
+    });
+
+    connectionRef.current = peer;
+  }
+
+  function* leaveCall() {
+    dispatch(setCall({ callEnded: true }));
+
+    connectionRef.current.destroy();
+
+    window.location.href = "/message";
+  }
+
   const handleClickCallUser = () => {
-    dispatch(callUser(id, userVideo, connectionRef));
+    // dispatch(callUser(id, userVideo, connectionRef));
+    callUser();
   };
 
   const handleClickHangUp = () => {
     socket?.emit("callEnded", { to: id });
-    dispatch(leaveCall(connectionRef));
+    // dispatch(leaveCall(connectionRef));
+    leaveCall();
   };
   const handleClickAnswer = () => {
-    dispatch(answerCall(userVideo, connectionRef));
+    // dispatch(answerCall(userVideo, connectionRef));
+    answerCall();
   };
   return (
     <div className="call">
